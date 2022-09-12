d654cfdc1612d6d32a2c48010c904c78e94b3b1f
HarshitM17 <mathurharshit88@gmail.com>
2022-09-12 15:17:56 +0530

    learning

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+var greet="Greetings my fellas";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hi I am Harshit Mathur
-        </a>
-      </header>
+    <>
+    <nav>
+    <li>Welcome</li>
+    <li>Information</li>
+    <li>Contact</li>
+    </nav>
+    <div className="blank">
+    <h1>Hi {greet}</h1>
+    <p>Welcome, to my website I am HArshit Mathur,Greeting from my side Thank you</p>
     </div>
+    </>
   );
 }
 
